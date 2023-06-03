5f50066933128990f1f8fb111810f115800003c3
sefayesilarslan <110385923+sefayesilarslan@users.noreply.github.com>
2023-06-03 09:44:43 +0300

    Update baglan.php

--- baglan.php
+++ baglan.php
@@ -1,6 +1,6 @@
 <?php 
 
- $baglan = new PDO("mysql:host=localhost;dbname=ogrenciler;charset=utf8", "root", "19221922");
+ $baglan = new PDO("mysql:host=localhost;dbname=ogrenciler;charset=utf8", "root", "şifre");
 $baglan->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $baglan->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 $baglan->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
